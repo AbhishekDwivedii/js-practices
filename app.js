e2cd24c10308f13b4533371ebbbedb347f6f8d34
Abhishek Dwivedi <Abhishek.Dwivedi@Coforge.com>
2026-05-01 10:58:29 +0530

    practice of filter & find methods

--- app.js
+++ app.js
@@ -9,33 +9,43 @@ app.get('/',(req, res)=>{
   res.sendFile(path.join(__dirname,'views','home.html'))
 });
 
+/// START PRACTICE FROM HERE----------
 
-// function name(first_name, last_name){
-//   console.log(`my name is,`+first_name);
-//   last_name();
-// }
-// function greet( wishing, noon ){
-//   console.log(`Dwivedi,`+wishing);
-//   noon();
-// }
-// function wish (){
-//   console.log(`Good afternoon!`)
-// }
-// const user = {
-//     first_name: 'ABHISHEK',
-//     last_name: 'dwivedi',
-//    greet: function(){
-//   console.log('good morning,', user.first_name, user.last_name);
-//  }
-  
-// }
-// user.greet();s
-let fruits = ['mango', 'orange', 'grapes', 'apple']
 
-// console.log(fruits)
-let result = fruits.slice()
 
-console.log(result);
+let students = [
+  { name: "Amit", marks: 85 },
+  { name: "Neha", marks: 90 },
+  { name: "Rahul", marks: 44 },
+  { name: "Priya", marks: 55 },
+  { name: "Arjuuun", marks: 70 }
+];
+
+let passedStud = students.find((element)=>{
+    return element.marks > 80;
+})
+
+console.log(passedStud);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
